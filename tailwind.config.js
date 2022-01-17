80f397b4df0026280e2934220642cbf5939c95a6
Welington Carlos <wcfilho98@hotmail.com>
2022-01-17 16:14:55 -0300

    Criação da página e separando as partes do layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        300: "75rem" /* 1200px */,
+        320: "80rem" /* 1280px */,
+      },
+    },
+  },
+  plugins: [],
+};
